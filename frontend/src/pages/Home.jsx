5a3003683eda6aa6bc277ee48da37209e17a7a0f
geekymca <kumarpankaj12830@gmail.com>
2024-08-03 01:38:42 +0530

    setting route and folder structure

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div style={{ height: "100px" }}>
+      <h1>Home</h1>
+      <p>Welcome to the Home page</p>
+    </div>
+  );
+}
+
+export default Home;
+
